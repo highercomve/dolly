192cbf7e9f3470998b58a3bfb49a99b72abe21a0
Sergio Marin <sergio.marin@pantacor.com>
2020-02-07 17:49:28 -0300

    Updates

---
+++ precache-manifest.9cbb0ec3ad15c3259f9ece7a43af994e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4f7738f9b9439f41adbe1637c2324c",
+    "url": "/dolly/index.html"
+  },
+  {
+    "revision": "f03cf6d65eebe04e2100",
+    "url": "/dolly/static/css/main.5db805e9.chunk.css"
+  },
+  {
+    "revision": "40d736b61994e6b03cbc",
+    "url": "/dolly/static/js/2.82517cbd.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/dolly/static/js/2.82517cbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f03cf6d65eebe04e2100",
+    "url": "/dolly/static/js/main.c9ab3429.chunk.js"
+  },
+  {
+    "revision": "d10dc3f4ce6918384252",
+    "url": "/dolly/static/js/runtime-main.c4d14f32.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.df2aef6434242ebfab6f55e0f46b1825.js"
+  "/dolly/precache-manifest.9cbb0ec3ad15c3259f9ece7a43af994e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dolly/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
