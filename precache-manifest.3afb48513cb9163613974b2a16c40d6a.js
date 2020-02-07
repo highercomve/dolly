45482f641e4d0bca623562ae01d6e226ebf71a7d
Sergio Marin <sergio.marin@pantacor.com>
2020-02-07 17:06:05 -0300

    Updates

---
+++ precache-manifest.3afb48513cb9163613974b2a16c40d6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69dc668b6d460ba3ee3010fd36427d3e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7f0e01858b547d8b28d",
+    "url": "/static/css/main.5db805e9.chunk.css"
+  },
+  {
+    "revision": "40d736b61994e6b03cbc",
+    "url": "/static/js/2.82517cbd.chunk.js"
+  },
+  {
+    "revision": "6f5f1c4a5f7f6e382b8aeca1b4ac8d96",
+    "url": "/static/js/2.82517cbd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7f0e01858b547d8b28d",
+    "url": "/static/js/main.31a2b37b.chunk.js"
+  },
+  {
+    "revision": "e61a3abdcf0cedfe02b9",
+    "url": "/static/js/runtime-main.8ad71ac7.js"
+  }
+]);
