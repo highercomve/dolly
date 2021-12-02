9488d6bf0c9133ea701d35fd88a04fb49af11b94
Sergio Marin <sergio.marin@pantacor.com>
2021-12-02 13:15:00 -0300

    Updates

---
+++ precache-manifest.1970828228ba9691877ddab9b5c59782.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e639078d283bf2aa4eeb83d7306df7c",
+    "url": "/dolly/index.html"
+  },
+  {
+    "revision": "1ad7a778d3e4d9292ea7",
+    "url": "/dolly/static/css/main.1979d274.chunk.css"
+  },
+  {
+    "revision": "5cf7a5159806af5dd0a3",
+    "url": "/dolly/static/js/2.6cd5be28.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/dolly/static/js/2.6cd5be28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ad7a778d3e4d9292ea7",
+    "url": "/dolly/static/js/main.0027173a.chunk.js"
+  },
+  {
+    "revision": "e9bd9971f0e755ea8429",
+    "url": "/dolly/static/js/runtime-main.64f4c6c8.js"
+  }
+]);
